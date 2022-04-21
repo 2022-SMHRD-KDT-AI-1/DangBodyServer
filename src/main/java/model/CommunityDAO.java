1e1a630381b596a403e38098b42c4efc1e72f7a7
smhrd <choic54195@gmail.com>
2022-04-21 14:29:18 +0900

    0421_deploy2

--- src/main/java/model/CommunityDAO.java
+++ src/main/java/model/CommunityDAO.java
@@ -109,7 +109,6 @@ public class CommunityDAO {
 			psmt = conn.prepareStatement(sql);		
 			psmt.setInt(1, seq);
 			
-			
 			cnt = psmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
