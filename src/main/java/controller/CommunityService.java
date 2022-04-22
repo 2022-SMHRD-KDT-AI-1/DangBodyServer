c09d97227c198b794f5506eeae7729b59df1ed1d
smhrd <choic54195@gmail.com>
2022-04-22 11:08:01 +0900

    220422 deploy

--- src/main/java/controller/CommunityService.java
+++ src/main/java/controller/CommunityService.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.commons.codec.binary.Base64;
 
 import model.CommunityDAO;
 
